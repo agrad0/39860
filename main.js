d22b6e9a19224e0229100f2dc6c96856495ae66f
agrad0 <grad.adam.j@gmail.com>
2022-08-19 11:14:37 +0200

    v1

---
+++ main.js
@@ -0,0 +1,12 @@
+let var1 = Number;
+let var2 = Number;
+function func(var1, var2){
+    if ((var1 == 20) || (var2 == 20)) {
+    return true;
+    } else if ((var1 += var2) <=20){ 
+    return true;
+    } else {
+    return false
+    }
+}
+
